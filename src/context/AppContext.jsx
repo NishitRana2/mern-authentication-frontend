f5e74ceadafce182438ae4756b2448078129ab12
NishitRana2 <127019417+NishitRana2@users.noreply.github.com>
2025-02-24 13:36:02 +0530

    Updated AppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,17 +1,50 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { toast } from 'react-toastify'; 
+
 
 export const AppContent = createContext() 
 
 export const AppContextProvider = (props)=>{
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    console.log("Backend URL:", backendUrl);
+    
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
+
+    const getAuthState = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/user/is-auth')
+            if(data.success) {
+                setIsLoggedin(true)
+                getUserData()
+            }
+        } catch (error) {
+            toast.error(error.response?.data?.message || 'An error occured')
+        }
+    }
+
+    const getUserData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/user/data')
+            data.success ? setUserData(data.userData) : toast.error(data.message)
+        } catch (error) {
+            toast.error(error.response?.data?.message || 'An error occurred')
+        }
+    }
+
+    useEffect(()=> {
+        getAuthState();
+    },[])
 
     const value = {
         backendUrl,
         isLoggedin, setIsLoggedin,
-        userData, setUserData 
+        userData, setUserData,
+        getUserData,
+        getAuthState
     }
 
     return(
